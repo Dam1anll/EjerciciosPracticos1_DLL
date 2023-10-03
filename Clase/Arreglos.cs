65e2fdce02281a1f883750e4367d23c2c255517f
Damian <damiianll20@gmail.com>
2023-10-03 13:20:24 -0700

    Agregar archivos de proyecto.

---
+++ Clase/Arreglos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPracticos1_DLL.Clase
+{
+    internal class Arreglos
+    {
+        public void MostrarArreglos() 
+        {
+            int[] arreglos = {2,4,6,8,10};
+            for(int x=0; x<arreglos.Length;x++) 
+            {
+                Console.WriteLine(arreglos[x]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EjerciciosPracticos1_DLL.Clase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPracticos1_DLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arreglos mostrarArreglos = new Arreglos();
+            mostrarArreglos.MostrarArreglos();
+        }
+    }
+}
